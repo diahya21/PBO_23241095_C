11268d0722106e850647a1cbe0c52e29bdd70a4e
diahya21 <165254225+diahya21@users.noreply.github.com>
2024-10-16 10:24:37 +0800

    Add files via upload

---
+++ 02-OBJECK.java
@@ -0,0 +1,51 @@
+Class Mahasiswa{
+    String Nama;
+    String NIM;
+    String Prodi;
+    Integer umur;
+    Double IPK;
+}
+
+Public class App {
+    Public static void main(String[] args) throws Exception {
+
+        Mahasiswa mhs_1 = new Mahasiswa();
+        Mhs_1.Nama = “Reymon”;
+        Mhs_1.NIM = “23241112Class Mahasiswa {
+            String Nama;
+            String NIM;
+            String Prodi;
+            Integer umur;
+            Double IPK;
+        }
+        
+        Public class App {
+            Public static void main(String[] args) throws Exception {
+        
+                Mahasiswa mhs_1 = new Mahasiswa();
+                Mhs_1.Nama = “Wiwin”;
+                Mhs_1.NIM = “23241099”;
+                Mhs_1.Prodi = “Pendidikan Teknologi Informasi”;
+                Mhs_1.umur = 21;
+                Mhs_1.IPK = 3.5;
+        
+                System.out.println(“Nama : “ + mhs_1.Nama);
+                System.out.println(“NIM : “ + mhs_1.NIM);
+                System.out.println(“Prodi : “mhs_1.Prodi);
+                System.out.println(“Umur : “ + mhs_1.umur);
+                System.out.println(“IPK : “ + mhs_1.IPK);
+            }
+        }
+        ”;
+        Mhs_1.Prodi = “Pendidikan Teknologi Informasi”;
+        Mhs_1.umur = 21;
+        Mhs_1.IPK = 3.5;
+
+        System.out.println(“Nama : “ + mhs_1.Nama);
+        System.out.println(“NIM : “ + mhs_1.NIM);
+        System.out.println(“Prodi : “mhs_1.Prodi);
+        System.out.println(“Umur : “ + mhs_1.umur);
+        System.out.println(“IPK : “ + mhs_1.IPK);
+    }
+}
+ 
